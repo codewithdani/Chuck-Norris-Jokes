8be0b5c72550bfc4cd89be8073e74d961c9c9051
Daniel <daniel@gmail.com>
2024-02-21 20:51:43 +0300

    Updated redirect URI to 'https://chuck-norris-jokes-762t.onrender.com/callback'

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="https://chuck-norris-jokes-762t.onrender.com"
+    redirect_uri="https://chuck-norris-jokes-762t.onrender.com/callback"
 )
 
 
